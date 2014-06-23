eebdc9e3e405db0f14346b9ac1001c145f9be881
EMiroshko <miro6ko@gmail.com>
2014-06-23 22:13:59 +0300

    change for actual weather for all towns

--- weather.js
+++ weather.js
@@ -1,6 +1,22 @@
-var weatherJ = $.getJSON( "http://api.openweathermap.org/data/2.5/weather?q=Berlin", function( data ) {
-  return(data)
-});
+function insertWeather(town){
+	$.getJSON( "http://api.openweathermap.org/data/2.5/weather?q="+town, function( data ) {
+		var temp_min=formatTemperature(data.main.temp_min);
+		var temp_max=formatTemperature(data.main.temp_max);
+		var temp_today=formatTemperature(data.main.temp);
+		 $(".high_low .today_parametr_data").html((temp_min)+"/"+(temp_max));
+		 $(".header h1").html(temp_today);
+		 $(".current_condition .today_parametr_data").html(data.weather[0].description);
+	  return(data)
+	});
+}
 
- //$(".today_parametr_data:first").html(weather.sys.sunrise);
- alert( weatherJ.sys.message );
+$("#choice_button").click(function(){
+	
+	insertWeather($("#enter_town").val());
+});//
+function formatTemperature(temp){
+	temp=Math.round(temp-273)+"°C";
+	return(temp);
+}
+
+ 
